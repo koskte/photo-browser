4cfac06d8b24f08697bf9104d2999f122aa899dc
koskte <koskinen6@gmail.com>
2021-01-08 00:46:42 +0200

    Finished the assignment

--- src/App.js
+++ src/App.js
@@ -1,49 +1,58 @@
-import './App.css';
-import { useState , useEffect } from 'react';
-import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import PhotoDetail from './Components/PhotoDetail';
-const _ = require('lodash');
+import "./App.css";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import PhotoDetail from "./Components/PhotoDetail";
+import Pagination from "react-js-pagination";
+
+const _ = require("lodash");
+
 function App() {
   const [photos, setPhotos] = useState([]);
   const [pageNum, setPageNum] = useState(1);
-
-  
+  const [photosPerPage] = useState(10);
 
   useEffect(() => {
-    async function fetchPhotos () {
-      const photoRespone = await fetch(`http://jsonplaceholder.typicode.com/photos?_page=${pageNum}`);
+    const fetchPhotos = async () => {
+      const photoRespone = await fetch(`http://jsonplaceholder.typicode.com/photos`);
       const photos = await photoRespone.json();
       setPhotos(photos);
-      console.log(photos)
       if (photos.length === 0) {
         setPageNum(1);
       }
-    }
+    };
     fetchPhotos();
-  }, [pageNum]);
-  
+  }, []);
+
+  const pageLastPhoto = pageNum * photosPerPage;
+  const pageFirstPhoto = pageLastPhoto - photosPerPage;
+  const photosToShow = photos.slice(pageFirstPhoto, pageLastPhoto);
+
   const thumbnails = () => {
-    return _.map(photos, (photo) => {
-    return ( 
-      <Link key={photo.id} 
-        to={{
-          pathname: `/${photo.id}`,
-          state: {fromAppComponent: photo}
-        }}>
-        <img src={photo.thumbnailUrl} alt="thumbnail" />
-      </Link>
-    )
-    })
-  }
+    return _.map(photosToShow, (photo) => {
+      return (
+        <Link key={photo.id} to={`/${photo.id}`}>
+          <img src={photo.thumbnailUrl} alt="thumbnail" className="Thumbnail" />
+        </Link>
+      );
+    });
+  };
+
   return (
-  <Router>
-    <div className="App">
-      <Route exact path="/:id" component={PhotoDetail}/>
-      {thumbnails()}
-      <button onClick={() => {setPageNum(pageNum-1)}}>prev page</button>
-      <button onClick={() => {setPageNum(pageNum+1)}}>next page</button>
-    </div>
-  </Router>
+    <Router>
+      <div className="App">
+        <Route exact path="/:id" component={PhotoDetail} />
+        <div className="ThumbnailsContainer">{thumbnails()}</div>
+        <div className="PaginationContainer">
+          <Pagination
+            activePage={pageNum}
+            itemsCountPerPage={photosPerPage}
+            totalItemsCount={photos.length}
+            pageRangeDisplayed={5}
+            onChange={(e) => setPageNum(e)}
+          />
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/Components/PhotoDetail.js
+++ src/Components/PhotoDetail.js
@@ -1,17 +1,81 @@
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import ReactDom from "react-dom";
+import "./PhotoDetail.css";
 
-import './PhotoDetail.css';
 function PhotoDetail({ location }) {
-    console.log(location)
-    const singlePhoto = location.state.fromAppComponent;
-    return (
-        <div className="Container">
-            <div className="Content">
-                <p>{location.state.fromAppComponent.title}</p>
-                <img className="center" src={singlePhoto.url} alt="large" />
-            </div>
-        </div>
-    );
-}
+  const [user, setUser] = useState({});
+  const [album, setAlbum] = useState({});
+  const [showUserInfo, setShowUserInfo] = useState(false);
+  const [singlePhoto, setSinglePhoto] = useState({});
+  const [doneLoading, setDoneLoading] = useState(false);
+  const history = useHistory();
 
+  useEffect(() => {
+    const fetchPhotoAlbumAndUser = async () => {
+      const singlePhotoPromise = await fetch(`http://jsonplaceholder.typicode.com/photos${location.pathname}`);
+      const singlePhoto = await singlePhotoPromise.json();
+      setSinglePhoto(singlePhoto);
+
+      const albumPromise = await fetch(`http://jsonplaceholder.typicode.com/albums/${singlePhoto.albumId}`);
+      const album = await albumPromise.json();
+      setAlbum(album);
+
+      const userPromise = await fetch(`http://jsonplaceholder.typicode.com/users/${album.userId}`);
+      const user = await userPromise.json();
+      setUser(user);
+      setDoneLoading(true);
+    };
+
+    fetchPhotoAlbumAndUser();
+  }, [singlePhoto.albumId, location.pathname]);
+
+  return ReactDom.createPortal(
+    <div className="Container">
+      {doneLoading ? (
+        <div className="Content">
+          <button onClick={() => history.goBack()} className="BackButton">
+            Back
+          </button>
+          <div className="ImageContainer">
+            <h3>{singlePhoto.title}</h3>
+            <img className="Image" src={singlePhoto.url} alt="large" />
+          </div>
+
+          <div className="ImageDetail">
+            <div className="BasicInfo">
+              <h3>From album: {album.title}</h3>
+              <h4 onClick={() => setShowUserInfo(!showUserInfo)}>
+                From user: <p className="UserName">{user.name}</p>
+              </h4>
+            </div>
+
+            {showUserInfo && (
+              <div className="UserInfo">
+                <div className="ContactInfo">
+                  <strong>Contact Info:</strong>
+                  <p>{user.name}</p>
+                  <p>{user.phone}</p>
+                  <p>{user.email}</p>
+                </div>
+
+                <div className="CompanyInfo">
+                  <strong>Company:</strong>
+                  <p>{user.company.name}</p>
+                  <p>{user.company.catchPhrase}</p>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      ) : (
+        <div className="Content">
+          <h1>Loading Image...</h1>
+        </div>
+      )}
+    </div>,
+    document.getElementById("portal")
+  );
+}
 
 export default PhotoDetail;
